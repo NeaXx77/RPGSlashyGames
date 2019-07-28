da06af5bfd2806c86bf04d8f1360aff19ace9cc2
NeaXx77 <46229846+NeaXx77@users.noreply.github.com>
2019-07-28 23:28:14 +0200

    Add files via upload

---
+++ Health.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RpgEssential
+{
+    public class BaseBar<T>{
+        //The base of all bars with function definition and virtual function
+        protected T maxAmount;
+        protected T currentAmount;
+    }
+    public class BarContainer<T>: BaseBar<T>{
+        //modular bar that could contain any type of contain (other than health)
+    }
+    //Do Health and an other modular bar class
+    public class Health<T>: BaseBar<T>{
+        public Health(T maxAmount, T currentAmount){
+            this.maxAmount = maxAmount;
+            this.currentAmount = currentAmount;
+        }
+        public void SetmaxAmount(T amount){
+            this.maxAmount = amount;
+        }
+        public void SetcurrentAmount(T amount){
+            currentAmount = amount;
+        }
+        public T GetcurrentAmount(){
+            return this.currentAmount;
+        }
+        public void TakeDamage(Health<int> health, int amount){
+            if(health.GetcurrentAmount() > amount){
+                health.SetcurrentAmount(health.GetcurrentAmount()-amount);
+            }else
+            {
+                health.SetcurrentAmount(0);
+            }
+        }
+    }
+}
